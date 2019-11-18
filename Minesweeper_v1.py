bfa874764b54eee23b812f8b58a1bb5f90e5d86e
Brett Regnier <bretternestregnier@gmail.com>
2019-11-18 15:14:46 -0700

    updating the folder structure

---
+++ Minesweeper_v1.py
@@ -0,0 +1,44 @@
+import gym
+import numpy as np
+from gym import spaces
+
+import pygame
+
+class Minesweeper_v1(gym.Env):
+    def __init__(self):
+        super(Minesweeper_v1, self).__init__()
+        
+    def Init(self):
+        pass
+    
+    def InitGraphics(self):
+        pass
+    
+    def InitGame(self):
+        pass
+    
+    def Update(self):
+        pass
+    
+    def Draw(self):
+        pass
+    
+    def MouseHover(self):
+        pass
+    
+    def Events(self):
+        pass
+    # TODO event handler
+    
+    def Click(self):
+        pass
+    # TODO cursor
+    
+    def Restart(self, hard):
+        pass
+    
+    def Play(self):
+        pass
+    
+    def Gameover(self):
+        pass
