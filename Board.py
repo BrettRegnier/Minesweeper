586f444a10a68f38d73597014a9c6229b28a0843
Brett Regnier <bretternestregnier@gmail.com>
2019-11-19 17:56:29 -0700

    building board and tile

--- Board.py
+++ Board.py
@@ -1,4 +1,7 @@
+import random
+
 from Tile import Tile
+
 
 class Board():
 	def __init__(self, x, y, w, h, rows, columns, mines):
@@ -14,33 +17,128 @@ class Board():
 		self._tiles = []
 		self._state = []
 		
-		self._unrevealed = 0
+		self._unrevealed = 0 # tiles that are not mines that are unrevealed should be updtated in click
 
 	def CreateBoard(self):
 		x = 0
 		y = 0
 		size = 32
 		total = self._rows * self._columns
+		self._unrevealed = total - self._mines
+		
 		count = 0
 		
 		# create tiles 1-d
 		for r in range(self._rows):
 			for c in range(self._columns):
-				self._tiles.append(Tile(self._x + x, self._y + y, size))
+				tile = Tile(self._x + x, self._y + y, size)
+				self._tiles.append(tile)
 				x = x + size
 				
-				self._state.append(10) # set the state to be all unrevealed - 10
+				self._state.append(tile.GetState()) # set the state to be all unrevealed - 10
 				count = count + 1
 
 			x = 0
 			y = y + size
+			
+		# Make mines
+		toMakeMine = self._mines
+		while (toMakeMine > 0):
+			tile = random.choice(self._tiles)
+			if (not tile.IsMine()):
+				tile.BeMine()
+				toMakeMine -= 1
+				
+		# Make tiles known to eachother
+		tpr = self._columns  # Tiles per row
+		count = len(self._tiles)
+		for i in range(count):
+			r = int(i / tpr)  # current row
+			c = int(i % tpr)  # current column
 
-	# might not be needed
-	def CreateTile(self):
-		pass
+			testidx = -1
 
+			# adjacent tiles
+			adj = []
+
+			# Northwest tile
+			NW = i - (tpr + 1)
+			if (NW >= 0 and int(NW / tpr) == r - 1):
+				adj.append(self._tiles[NW])
+				
+				if i == testidx:
+					print("NW" + str(NW))
+			else:
+				adj.append(None)
+
+			# North tile
+			N = i - tpr
+			if (N >= 0):
+				adj.append(self._tiles[N])
+				if i == testidx:
+					print("N" + str(N))
+			else:
+				adj.append(None)
+
+			# Northeast tile
+			NE = i - (tpr - 1)
+			if (NE >= 0 and int(NE / tpr) == r - 1):
+				adj.append(self._tiles[NE])
+				if i == testidx:
+					print("NE" + str(NE))
+			else:
+				adj.append(None)
+
+			# West tile
+			W = i - 1
+			if (W > 0 and int(W / tpr) == r):
+				adj.append(self._tiles[W])
+				if i == testidx:
+					print("W" + str(W))
+			else:
+				adj.append(None)
+				
+			# East tile
+			E = i + 1
+			if (E < count and int(E / tpr) == r):
+				adj.append(self._tiles[E])
+				if i == testidx:
+					print("E" + str(E))
+			else:
+				adj.append(None)
+
+			# Southwest tile
+			SW = i + (tpr - 1)
+			if (SW < count and int(SW / tpr) == r + 1):
+				adj.append(self._tiles[SW])
+				if i == testidx:
+					print("SW" + str(SW))
+			else:
+				adj.append(None)
+
+			# South tile
+			S = i + tpr
+			if (S < count):
+				adj.append(self._tiles[S])
+				if i == testidx:
+					print("S" + str(S))
+			else:
+				adj.append(None)
+			
+			# Southeast tile
+			SE = i + (tpr+1)
+			if (SE < count and int(SE / tpr) == r + 1):
+				adj.append(self._tiles[SE])
+				if i == testidx:
+					print("SE" + str(SE))
+			else:
+				adj.append(None)
+				
+			self._tiles[i].AdjacentTiles(adj)
+			
 	def Update(self, tick):
-		pass
+		for tile in self._tiles:
+			tile.Update(tick)
 
 	def Draw(self, screen, graphics):
 		pass
--- Tile.py
+++ Tile.py
@@ -4,6 +4,19 @@ class Tile():
 		self._y = y
 		self._size = size
 		self._id = tid
+		
+		self._mine = False
+		self._adjTiles = []
+		
+		self._revealed = False
+		self._flagged = False
+		self._state = -10
+		
+		self._outercolor = 255
+		self._innercolor = 200        
+		self._revealedcolor = 180
+		self._outline = 100
+		self._textcolor = (0, 0, 0)
 	
 	def Update(self, tick):
 		pass
@@ -19,3 +32,36 @@ class Tile():
 	
 	def Reset(self):
 		pass
+	
+	def IsMine(self):
+		return self._isMine
+		
+	def BeMine(self):
+		self._state = 11
+		self._mine = True
+		
+	def GetState(self):
+		return self._state
+		
+	def AdjacentTiles(self, adj):
+		self._adjTiles = adj
+		
+		for tile in self._adjacentTiles:
+			if (tile is not None and tile.IsMine()):
+				self._nearbyMines = self._nearbyMines + 1
+				
+		# skipping == 1 because its default color.
+		if self._nearbyMines == 2:
+			self._textcolor = (71, 92, 68)
+		elif self._nearbyMines == 3:
+			self._textcolor = (226, 78, 27)
+		elif self._nearbyMines == 4:
+			self._textcolor = (135, 61, 72)
+		elif self._nearbyMines == 5:
+			self._textcolor = (25, 12, 14)
+		elif self._nearbyMines == 6:
+			self._textcolor = (63, 48, 71)
+		elif self._nearbyMines == 7:
+			self._textcolor = (140, 75, 92)        
+		elif self._nearbyMines == 8:
+			self._textcolor = (53, 129, 184)
